2791629a2b013bb821d56f42ace693564aa7de41
Patrick Jennings <patrickaeb@gmail.com>
2016-01-31 19:43:18 -0500

    Rewrite of the xplanet script. Removed python script that downloaded cloud maps as the xplanet script now does this. Resized night image so that xplanet does not have to resize it everytime script is run. Updated README to reflect changes.

--- install.sh
+++ install.sh
@@ -1,32 +1,23 @@
 #!/bin/sh
-
 # Written by Patrick Jennings
-# install.sh v0.2
+# https://github.com/patrickjennings/XPlanet-Scripts
+# install.sh v0.3
 
-# Installs the xplanet scripts to the directory:
-# ${HOME}/.xplanet
-XPLANET_DIR=${HOME}/.xplanet
+# Installs the xplanet scripts into this directory.
+XPLANETDIR="$HOME/.xplanet"
 
-command -v xplanet >/dev/null 2>&1 || { echo "Xplanet is required to be installed. Aborting." >&2; exit 1; }
+echo "Initializing directory $XPLANETDIR"
+mkdir -p "$XPLANETDIR"
 
-echo "Initializing directories..."
-mkdir ${XPLANET_DIR}
+echo "Installing script into $XPLANETDIR"
+cp xplanet.sh "${XPLANETDIR}/"
 
-echo "Installing scripts..."
-cp config ${XPLANET_DIR}/
-cp xplanet-download_clouds.py ${XPLANET_DIR}/
-cp xplanet-gnome2.sh ${XPLANET_DIR}/
-cp xplanet.sh ${XPLANET_DIR}/
+echo "Copying images into $XPLANETDIR"
+cp -r world "${XPLANETDIR}/world"
 
-echo "Installing images..."
-cp -r world ${XPLANET_DIR}/world
+for FILE in "bump.jpg night.gif specular.jpg clouds.jpg";
+do
+    cp $FILE "${XPLANETDIR}/$FILE"
+done
 
-cp bump.jpg ${XPLANET_DIR}/bump.jpg
-cp night.gif ${XPLANET_DIR}/night.gif
-cp specular.jpg ${XPLANET_DIR}/specular.jpg
-cp clouds.jpg ${XPLANET_DIR}/clouds.jpg
-
-echo "**************"
-echo "**Completed!**"
-echo "**************"
-
+echo "Success!"
--- xplanet.sh
+++ xplanet.sh
@@ -1,38 +1,126 @@
-#
-# Written by Patrick Jennings
-#
-# This xplanet script can be used to automatically update a users
-# desktop background.
-#
-
 #!/bin/bash
+#
+# Patrick Jennings
+# This xplanet script can be used to generate a xplanet wallpaper.
+# See https://github.com/patrickjennings/XPlanet-Scripts
 
-homeDir=${HOME}
+# Exit on any failure.
+set -e
 
-# Usually the monitor size.
-geometry=1920x1200
+# Geometry of output image.
+GEOMETRY='1920x1200'
 
-# Where the xplanet config file is located
-configFile=$homeDir/.xplanet/config
+# Xplanet options. Read `man xplanet` for an explanation of each option.
+XOPTIONS='-num_times 1 -projection rectangular -quality 100 -light_time -verbosity -1'
 
-# The name of the desktop image
-outputFile=$homeDir/.xplanet/xplanet.png
+# Date commands. May be different if you don't use coreutils.
+TIMESTAMP=`date +%s`
+MONTH=`date +%m`
 
-# Here are some options you may want to edit. Read `man xplanet` for
-# a detailed explanation of each option.
-xoptions="-num_times 1 -longitude 0 -latitude 0 -body earth \
-	-projection rectangular -quality 100 -light_time -verbosity -1"
+# The name of the output image.
+OUTPUTFILE="$HOME/.xplanet/xplanet.jpg"
 
-# Get the correct earth map
-month=`/bin/date +%m`
-/bin/ln -sf $homeDir/.xplanet/world/${month}.jpg $homeDir/.xplanet/earth.jpg
+# Folder defines.
+SCRIPTFOLDER="$HOME/.xplanet"
 
-# Get the cloud map
-python2 $homeDir/.xplanet/xplanet-download_clouds.py \
-	$homeDir/.xplanet/clouds.jpg
+# Cloud image or empty to not use cloud maps.
+CLOUDFILE="$SCRIPTFOLDER/clouds.jpg"
+# Earth base map image or empty to use xplanet default image.
+MAPFILE="$SCRIPTFOLDER/world/$MONTH.jpg"
+# Night image or empty.
+NIGHTFILE="$SCRIPTFOLDER/night.gif"
+# Bump map image or empty.
+BUMPFILE="$SCRIPTFOLDER/bump.jpg"
+# Specular map image or empty.
+SPECULARFILE="$SCRIPTFOLDER/specular.jpg"
 
-# Run xplanet
-xplanet -output $outputFile -geometry $geometry -config $configFile \
-	$xoptions
+# Cloud map mirror or empty to never download cloud maps.
+CLOUDMIRROR='http://xplanetclouds.com/free/local/clouds_2048.jpg'
+
+# Amount of hours to wait to download a new cloud map. 0 to always download.
+# Some mirrors will limit the amount of downloads per day. Be considerate!
+DOWNLOADFREQ=12
+
+# File to use for the temporary xplanet config.
+# This file will be overwritten and deleted.
+XCONFIGFILE=`mktemp`
+
+
+# Internal functions.
+
+# exit with or without an error message.
+exit_gracefully () {
+    if [ ! -z "$1" ];
+    then
+        echo "$1" >&2
+    fi
+    exit 1
+}
+
+# $1 is the config name.
+# $2 is the file.
+set_config_if_set () {
+    if [ ! -z "$2" ];
+    then
+        if [ ! -f "$2" ];
+        then
+            exit_gracefully "Failed to locate $1 file $2"
+        fi
+
+        echo "$1=$2" >> "$XCONFIGFILE"
+    fi
+}
+
+
+# Check that dependencies are installed.
+DEPENDENCIES='xplanet curl'
+for DEP in $DEPENDENCIES;
+do
+    command -v $DEP >/dev/null || exit_gracefully "$DEP is required to be installed."
+done
+
+# Populate xplanet config with earth section.
+echo -e '[earth]\n"Earth"' > "$XCONFIGFILE"
+
+# Add map file to config if set.
+set_config_if_set 'map' "$MAPFILE"
+# Add night file to config if set
+set_config_if_set 'night_map' "$NIGHTFILE"
+# Add bump file to config if set.
+set_config_if_set 'bump_map' "$BUMPFILE"
+# Add specular file to config if set.
+set_config_if_set 'specular_map' "$SPECULARFILE"
+
+# Add cloud map to config if set.
+if [ ! -z "$CLOUDFILE" ];
+then
+    # Download cloud map if mirror is set and it has been over DOWNLOADFREQ time.
+    if [ ! -z "$CLOUDMIRROR" ];
+    then
+        # Check if cloud map needs to be downloaded.
+        SECONDS=`expr $DOWNLOADFREQ \* 3600`
+        UTIME=`expr $SECONDS + 1`
+        if [ -f "$CLOUDFILE" ];
+        then
+            MTIME=`stat -c %Y "$CLOUDFILE"`
+            UTIME=`expr $TIMESTAMP \- $MTIME`
+        fi
+
+        if [ $UTIME -gt $SECONDS ]
+        then
+            # Get the cloud map.
+            curl -s -S "$CLOUDMIRROR" -o "$CLOUDFILE"
+        fi
+    fi
+
+    # Add cloud map to config if set.
+    set_config_if_set 'cloud_map' "$CLOUDFILE"
+fi
+
+# Run xplanet.
+xplanet -geometry "$GEOMETRY" -output "$OUTPUTFILE" -config "$XCONFIGFILE" $XOPTIONS
+
+# Delete temporary config file.
+rm "$XCONFIGFILE"
  
-echo $outputFile
+#echo "$OUTPUTFILE"
